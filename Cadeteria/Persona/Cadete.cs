d9e1a56156c93a1749a4465e1a278f5fcab5095d
Agustin978 <loboagus10@gmail.com>
2022-10-13 11:57:15 -0300

    Ingresado metodo para mostrar el pago total por pedidos entregados y metodo para agregar nuevos pedidos al cadete e ingresados los logger para guardar la informacion cuando se agrega un nuevo pedido y cuando el cadete entrega un pedido

--- Cadeteria/Persona/Cadete.cs
+++ Cadeteria/Persona/Cadete.cs
@@ -1,9 +1,31 @@
+using NLog;
 class Cadete : Persona
 {
     private float jornalACobrar;
-    //private List<Pedido> pedidos; Debo recibir un listado de pedidos
-    public Cadete(string Nombre, string Direccion, int telefono):base(Nombre, Direccion, telefono)
+    private List<Pedido> pedidos;
+    private Logger logger = LogManager.GetCurrentClassLogger();
+    public Cadete(string Nombre, string Direccion, int telefono, List<Pedido> pedidos):base(Nombre, Direccion, telefono)
     {
+        this.pedidos = pedidos;
+    }
+    public Pedido getPedidos(int pedido){return this.pedidos[pedido];}
+    public float getJornal(){return this.jornalACobrar;}
 
+    private void calculaJornalTotal()
+    {
+        foreach(var pedido in this.pedidos)
+        {
+            if(string.Equals(pedido.getEstadoPedido(),"Entregado"))
+            {
+                this.jornalACobrar += 300;
+                this.logger.Trace($"El cadete {this.getNombre()}, con id {this.getID()}, realizo una entrega");
+            }
+        }
+    }
+
+    public void agregaPedido(Pedido p)
+    {
+        this.pedidos.Add(p);
+        this.logger.Trace($"Se ingreso un nuevo pedido al cadete {this.getNombre()}");
     }
 }
