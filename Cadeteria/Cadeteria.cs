593c267bfda3ca8da45ac9cc193cccb508a81bb4
Agustin978 <loboagus10@gmail.com>
2022-10-14 01:53:03 -0300

    Creada la clase cadeteria donde se almacenran todos los cadetes pertenecientes a la misma

---
+++ Cadeteria/Cadeteria.cs
@@ -0,0 +1,27 @@
+class Cadeteria
+{
+    private string Nombre {get;set;}
+    private string telefono {get;set;}
+    private List<Cadete> cadetes;
+
+    public Cadeteria(string nombre, string telefono, List<Cadete> cadetes)
+    {
+        this.Nombre = nombre;
+        this.telefono = telefono;
+        this.cadetes = cadetes;
+    }
+
+    public void MuestraCadetes()
+    {
+        foreach(var cadete in this.cadetes)
+        {
+            Console.WriteLine($"Nombre cadete: {cadete.getNombre}");
+            Console.WriteLine($"Direccion: {cadete.getDireccion}");
+            Console.WriteLine($"ID: {cadete.getID()}");
+            Console.WriteLine($"Telefono: {cadete.getTelefono()}");
+            Console.WriteLine($"Monto a cobrar: {cadete.getJornal()}");
+            Console.WriteLine($"Muestra pedidos a su cargo");
+            cadete.muestraPedidos();
+        }
+    }
+}
